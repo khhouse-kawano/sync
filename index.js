f0872d847801143c07aa114a1257906f29fa0264
kawano-kh <shinji.kawano@kh-group.jp>
2025-05-27 17:08:38 +0900

    cors()を適用

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { chromium } = require("playwright-chromium");
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
