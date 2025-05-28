73cd4f6989b3d8c97e94ef7a14248e18e4fc2a0f
kawano-kh <shinji.kawano@kh-group.jp>
2025-05-28 14:03:38 +0900

    Delete timeout

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.post("/", async (req, res) => {
 
     const main = async (mail, password) => {
         try {
-            page.setDefaultTimeout(300000); // 5分にタイムアウトを設定
-
             await login(mail, password);
             await fillForm();
             await detailSave();
