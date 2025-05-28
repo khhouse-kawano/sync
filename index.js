6ba409673b389250f3fc53d5383bec9b7af40131
kawano-kh <shinji.kawano@kh-group.jp>
2025-05-28 12:48:37 +0900

    add code

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ app.post("/", async (req, res) => {
     const page = await context.newPage();
 
     const registerDate = req.body;
+    let pg_id;
 
     const main = async (mail, password) => {
         try {
             await login(mail, password);
             await fillForm();
+            await detailSave();
+            await dataCheck();
 
             await browser.close();
         } catch (error) {
@@ -33,11 +36,12 @@ app.post("/", async (req, res) => {
         await page.fill('#form_password', password);
         await page.click('//html/body/main/div/form[1]/div/div[2]/input[2]');
         await page.waitForLoadState('networkidle');
-    }
+    };
 
     const fillForm= async ()=> {
         await page.click('//html/body/main/div/div[2]/div[1]/div[2]/div[7]/a');
         await page.waitForLoadState('networkidle');
+        pg_id = await page.$eval('//html/body/main/div[1]/div[2]/div/form/div[1]/div[1]/div[1]/div[2]/span', el=>el.textContent.trim())
         if (registerDate.firstName ) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[4]/div[1]/div[2]/input[1]', String(registerDate.firstName)); // 姓
         if (registerDate.lastName) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[4]/div[1]/div[2]/input[2]', String(registerDate.lastName)); // 名
         if (registerDate.firstKana) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[5]/div[1]/div[2]/input[1]', String(registerDate.firstKana)); // セイ
@@ -84,14 +88,85 @@ app.post("/", async (req, res) => {
         await page.click('//html/body/main/div/div[2]/div/form/div[3]/div[2]/div/button'); // 登録ボタン
         // await page.waitForTimeout(4500); // 4秒待機 ※詳細編集画面が現れないため
         await page.waitForLoadState('networkidle');
-    }
+    };
+
+    const detailSave = async() =>{
+        await page.click('//html/body/main/div/div[2]/div/form/div[3]/div[2]/div/a[1]'); // 詳細編集画面へ
+        await page.waitForLoadState('networkidle');
+
+        // 店舗入力
+        if ( registerDate.shop && !registerDate.shop.includes('店舗未設定') ) {
+            await page.click('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]'); // 店舗ドロップダウンを表示
+            const shopValue = registerDate.shop.includes('PGH') ? 'PG HOUSE宮崎店' : registerDate.shop;
+            await page.fill('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', String( shopValue )); // 店舗名を入力
+            await page.waitForSelector(`div[data-label="${shopValue}"]`);
+            await page.click(`div[data-label="${shopValue}"]`);
+            const selectedShop = await page.$eval('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', el => el.value);
+            if ( selectedShop === '' ) {
+                await page.click('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]'); // 店舗ドロップダウンを表示
+                const shopValue = registerDate.shop.includes('PGH') ? 'PG HOUSE宮崎店' : registerDate.shop;
+                await page.fill('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', String( shopValue )); // 店舗名を入力
+                await page.waitForSelector(`div[data-label="${shopValue}"]`);
+                await page.click(`div[data-label="${shopValue}"]`);
+            }
+        }
+
+        // 日付入力
+        if ( registerDate.inquiry_date ) {
+            const formattedDate = registerDate.inquiry_date.replace(/\//g, '-');
+            await page.fill('#calendar_item_0_scheduled_at', formattedDate);
+            await page.waitForTimeout(500);  
+            await page.fill('#calendar_item_0_start_at', formattedDate);
+            await page.waitForTimeout(500); 
+            const formattedDateRetry = await page.$eval('#calendar_item_0_start_at', el => el.value);
+            const selectedShopRetry = await page.$eval('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', el => el.value);
+            if ( formattedDateRetry === "" ){
+                await page.fill('#calendar_item_0_scheduled_at', formattedDate);
+                await page.waitForTimeout(500);  
+            }
+            if (selectedShopRetry === "" ){
+                await page.fill('#calendar_item_0_start_at', formattedDate);
+                await page.waitForTimeout(500); 
+            }
+        }
+
+        await page.click('//html/body/main/div[1]/div[3]/form/div[4]/div[2]/div[2]/div[1]/button'); // 保存ボタン
+        await page.waitForTimeout(1000); // 
+    };
+
+    const dataCheck = async() => {
+        const selectedShop = await page.$eval('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', el => el.value);
+        if ( selectedShop === '' ) {
+            await page.click('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]'); // 店舗ドロップダウンを表示
+            const shopValue = registerDate.shop.includes('PGH') ? 'PG HOUSE宮崎店' : registerDate.shop;
+            await page.fill('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', String( shopValue )); // 店舗名を入力
+            await page.waitForSelector(`div[data-label="${shopValue}"]`);
+            await page.click(`div[data-label="${shopValue}"]`);
+        }
+
+        const formattedDateRetry = await page.$eval('#calendar_item_0_start_at', el => el.value);
+        const selectedShopRetry = await page.$eval('//html/body/main/div[1]/div[3]/form/div[2]/div[1]/div[11]/div[1]/input', el => el.value);
+        if ( formattedDateRetry === "" ){
+            await page.fill('#calendar_item_0_scheduled_at', formattedDate);
+            await page.waitForTimeout(500);  
+        }
+        if (selectedShopRetry === "" ){
+            await page.fill('#calendar_item_0_start_at', formattedDate);
+            await page.waitForTimeout(500); 
+        }
+    };
     
     const pg_mail = process.env.MAIL;
     const pg_pass = process.env.PASS;
 
     await main(pg_mail, pg_pass);
 
-    res.send("処理が完了");
+    const url = `https://pg-cloud.jp/customers/${ pg_id }/summary`
+
+    res.send({
+        "message" : "処理が終了",
+        "pg_id" : url
+    });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
