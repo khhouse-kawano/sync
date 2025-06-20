035b7b868782d479b8c60576615e2941ef91bd6c
kawano-kh <shinji.kawano@kh-group.jp>
2025-06-20 21:29:07 +0900

    m

--- index.js
+++ index.js
@@ -302,7 +302,7 @@ const runDataUpdate = async (updateData, shopValue, pg_mail, pg_pass) => {
 
         if( updateData.register && updateData.register !== '') {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/input', updateData.register);
-        } else if( updateData.register && updateData.register === ''){
+        } else if( updateData.register === ''){
             await page.evaluate(() => {
                 const el = document.getElementById('calendar_item_0_start_at');
                 if (el) el.value = '';
@@ -311,7 +311,7 @@ const runDataUpdate = async (updateData, shopValue, pg_mail, pg_pass) => {
 
         if( updateData.reserve && updateData.reserve !== '') {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[1]/div[3]/div[2]/input', updateData.reserve);
-        } else if( updateData.reserve && updateData.reserve === '') {
+        } else if( updateData.reserve === '') {
             await page.evaluate(() => {
                 const el = document.getElementById('calendar_item_2_start_at');
                 if (el) el.value = '';
@@ -320,7 +320,7 @@ const runDataUpdate = async (updateData, shopValue, pg_mail, pg_pass) => {
 
         if( updateData.line_group && updateData.line_group !== '') {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[1]/div[4]/div[2]/input', updateData.line_group);
-        } else if( updateData.line_group && updateData.line_group === '') {
+        } else if(  updateData.line_group === '') {
             await page.evaluate(() => {
                 const el = document.getElementById('calendar_item_3_start_at');
                 if (el) el.value = '';
@@ -329,7 +329,7 @@ const runDataUpdate = async (updateData, shopValue, pg_mail, pg_pass) => {
 
         if( updateData.screening && updateData.screening !== '') {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[1]/div[6]/div[2]/input', updateData.screening); 
-        } else if( updateData.screening && updateData.screening === ''){
+        } else if(  updateData.screening === ''){
             await page.evaluate(() => {
                 const el = document.getElementById('calendar_item_5_start_at');
                 if (el) el.value = '';
@@ -338,20 +338,11 @@ const runDataUpdate = async (updateData, shopValue, pg_mail, pg_pass) => {
 
         if( updateData.appointment && updateData.appointment !== '') {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[1]/div[9]/div[2]/input', updateData.appointment);
-        } else if( updateData.appointment && updateData.appointment === ''){
-  const input = page.locator('#calendar_item_8_start_at');
-  await input.click(); // フォーカスを当てる
-
-  await page.keyboard.press('Delete');
-  await page.keyboard.press('ArrowRight');
-  await page.keyboard.press('Delete');
-  await page.keyboard.press('ArrowRight');
-  await page.keyboard.press('Delete');
-  await page.keyboard.press('ArrowLeft');
-  await page.keyboard.press('Delete');
-  await page.keyboard.press('ArrowLeft');
-  await page.keyboard.press('Delete');
-
+        } else if( updateData.appointment === ''){
+            await page.evaluate(() => {
+                const el = document.getElementById('calendar_item_8_start_at');
+                if (el) el.value = '';
+            });
         }
 
         await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/div/div[2]/div[2]/div[2]/button[1]');
