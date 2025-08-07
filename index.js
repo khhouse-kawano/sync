f868e3449a197a29cda52cee62a1ef686a0de45b
kawano-kh <shinji.kawano@kh-group.jp>
2025-08-07 21:13:38 +0900

    fix

--- index.js
+++ index.js
@@ -130,13 +130,13 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
     const fillForm = async () => {
         await page.click('//html/body/main/div/div[2]/div[1]/div[2]/div[7]/a');
         await page.waitForLoadState('networkidle');
-        console.log('ログイン成功');
+        await console.log('ログイン成功');
         if (registerData.firstName) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[4]/div[1]/div[2]/input[1]', String(registerData.firstName));
         if (registerData.lastName) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[4]/div[1]/div[2]/input[2]', String(registerData.lastName));
         if (registerData.firstKana) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[5]/div[1]/div[2]/input[1]', String(registerData.firstKana));
         if (registerData.lastKana) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[5]/div[1]/div[2]/input[2]', String(registerData.lastKana));
         if (registerData.name) await page.fill('//html/body/main/div/div[2]/div/form/div[1]/div[4]/div[1]/div[2]/input[1]', String(registerData.name));
-        console.log('名前の入力完了');
+        await console.log('名前の入力完了');
 
         if (registerData.medium) {
             let mediumValue;
@@ -150,13 +150,13 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
             await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[2]/div[2]/div/div/div[1]');
             await page.click(`div[data-label="${mediumValue}"]`);
         }
-        console.log('販促媒体の入力完了');
+        await console.log('販促媒体の入力完了');
 
         if ( registerData.staff ) {
             await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[3]/div[3]/div[2]/div/div[1]');
             await page.click(`div[data-label="${registerData.staff}"]`);
         }
-        console.log('担当営業の入力完了');
+        await console.log('担当営業の入力完了');
 
         await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[6]/div[1]/div[2]/div/div[1]');
 
@@ -169,7 +169,7 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
 
         if (registerData.mail && registerData.mail.includes('@')) await page.fill('#customer_customer_contacts_attributes_0_email', String(registerData.mail));
         await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[6]/div[1]/div[2]/div/div[2]/div[2]/div[2]/button[1]');
-        console.log('連絡先の入力完了');
+        await console.log('連絡先の入力完了');
 
         await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[6]/div[2]/div[2]/div/div[1]');
         if (registerData.zip) {
@@ -198,7 +198,7 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
         }
 
         await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[6]/div[2]/div[2]/div/div[2]/div[2]/div[2]/button[1]');
-        console.log('住所の入力完了');
+        await console.log('住所の入力完了');
 
         // 名簿取得日を入力
         if ( registerData.date){
@@ -207,7 +207,7 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/turbo-frame/div/div[2]/div[2]/div[1]/div[1]/div[2]/input', formattedDate);
             await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[16]/div[1]/div/turbo-frame/div/div[2]/div[2]/div[2]/button[1]');
         }
-        console.log('名簿取得日の入力完了');
+        await console.log('名簿取得日の入力完了');
 
 
         // 事前アンケート
@@ -216,7 +216,7 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[4]/div[3]/div[2]/div/div[2]/div[2]/div[1]/textarea', registerData.survey);
             await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[4]/div[3]/div[2]/div/div[2]/div[2]/div[2]/button[1]');
         }
-        console.log('事前アンケートの入力完了');
+        await console.log('事前アンケートの入力完了');
 
         // 商談メモ
         if ( registerData.note && registerData.note !== ''){
@@ -225,11 +225,12 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
             await page.fill('//html/body/main/div[1]/div[2]/div/form/div[1]/div[14]/div/div/div/div[2]/div[2]/div[1]/textarea', newNote);
             await page.click('//html/body/main/div[1]/div[2]/div/form/div[1]/div[14]/div/div/div/div[2]/div[2]/div[2]/button[1]');
         }  
-        console.log('備考欄の入力完了');
+        await console.log('備考欄の入力完了');
 
 
         await page.click('//html/body/main/div/div[2]/div/form/div[3]/div[2]/div/button');
-        const startTime = Date.now();
+        await console.log('保存ボタン');
+
 
         while (true) {
             pg_id = await page.url();
