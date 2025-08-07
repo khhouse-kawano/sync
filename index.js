71c395101c68c421879286a881e5ad78aaaeae11
kawano-kh <shinji.kawano@kh-group.jp>
2025-08-07 21:24:08 +0900

    fix

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ const runDataRegistration = async (registerData, pg_mail, pg_pass) => {
 
         await page.click('//html/body/main/div/div[2]/div/form/div[3]/div[2]/div/button');
         console.log('保存ボタン');
-        await page.waitForTimeout(4500); // 詳細編集画面が現れるまで待機
+        await page.waitForTimeout(7500); // 詳細編集画面が現れるまで待機
         await page.waitForLoadState('networkidle');
 
         pg_id = await page.url();
