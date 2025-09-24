a4d4307688a0c904cb56bc4e8925dddf2d55a0aa
kawano-kh <shinji.kawano@kh-group.jp>
2025-09-24 11:32:52 +0900

    add analytics

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const runDataUpdateAfterInterview = require("./runDataUpdateAfterInterview.js");
 const runMyHomeRobo = require("./runMyHomeRobo.js");
 const runBeforeSurvey = require("./runBeforeSurvey.js");
 const runDataUpdateNew = require("./runDataUpdateNew.js");
+const { use } = require("react");
 
 const app = express();
 app.use(cors());
@@ -177,5 +178,57 @@ app.post("/api/before_survey", async (req, res) => {
     runBeforeSurvey(updateData, shopValue, pg_mail, pg_pass)
   );
 });
+
+app.get("/open", async (req, res) => {
+  const userId = req.query.id;
+  const now = new Date();
+
+  console.log(`Opened by ${userId} at ${now}`);
+
+  // POSTするデータ
+  const postData = {
+    demand: "open_myhomerobo_mail",
+    id: userId,
+    ua: req.headers["user-agent"],
+    ip: req.ip
+  };
+
+  try {
+    const headers = {
+      Authorization: "4081Kokubu",
+      "Content-Type": "application/json",
+    };
+
+    await axios.post("https://khg-marketing.info/dashboard/api/", postData, {
+      headers,
+    });
+
+    console.log("開封ログ送信: 成功");
+  } catch (error) {
+    if (error.response) {
+      console.error("APIエラー:", error.response.status, error.response.data);
+    } else {
+      console.error("通信エラー:", error.message);
+    }
+  }
+
+  // 1x1透明PNG
+  const img = Buffer.from(
+    "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAoMBgX9nX9sAAAAASUVORK5CYII=",
+    "base64"
+  );
+
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": img.length,
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    "Pragma": "no-cache",
+    "Expires": "0",
+  });
+
+  res.end(img);
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
