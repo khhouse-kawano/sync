918fcf1ba6e6d3d0c0ebf1f1af9bb1b89d5108d3
kawano-kh <shinji.kawano@kh-group.jp>
2026-02-04 11:32:08 +0900

    fix

--- dist/routes/indexRoutes.js
+++ dist/routes/indexRoutes.js
@@ -3,5 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const syncControllers_1 = require("../controllers/syncControllers");
 const router = (0, express_1.Router)();
+router.options("/", (req, res) => {
+    res.sendStatus(200);
+});
 router.post("/", syncControllers_1.syncController.handleSync);
 exports.default = router;
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -2,5 +2,10 @@ import { Router } from "express";
 import { syncController } from "../controllers/syncControllers";
 
 const router = Router();
+
+router.options("/", (req, res) => {
+    res.sendStatus(200);
+});
+
 router.post("/", syncController.handleSync);
 export default router;
